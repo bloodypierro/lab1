9e0565e0aa045093c9095055563971860007c053
bloodypierro <internetwarningg@gmail.com>
2025-09-26 11:04:42 +0200

    Add files via upload

--- lab1.cpp
+++ lab1.cpp
@@ -1,20 +1,21 @@
 ﻿
-#include <iostream>
-#include <cmath>
-using namespace std;
+    #include <iostream>
+    #include <cmath>
+    using namespace std;
 
 
-int main()
-{
-    const double P = 3.14;
-    double radius;
-    cout << "Radius sferi:";
-    cin >> radius;
-    double Ploshyad = 4 * P * pow(radius, 2);
-    double volume = (4.0 / 3.0) * P * pow(radius, 3);
-    cout << "\n Result:" << endl;
-    cout << "Radius sferi:" << radius << endl;
-    cout << "Ploshyad poverhnosti:" << Ploshyad << endl;
-    cout << "Obiem sferi:" << volume << endl;
-    return 0;
-}
+    int main()
+    {
+        setlocale(LC_ALL, "Russian");
+        const double P = 13.1415;
+        double radius;
+        cout << "Радиус сферы:";
+        cin >> radius;
+        double Ploshyad = 4 * P * pow(radius, 2);
+        double volume = (4.0 / 3.0) * P * pow(radius, 3);
+        cout << "\n Результат:" << endl;
+        cout << "Радиус сферы:" << radius << endl;
+        cout << "Площадь поверхности:" << Ploshyad << endl;
+        cout << "Обьем сферы:" << volume << endl;
+        return 0;
+    }
