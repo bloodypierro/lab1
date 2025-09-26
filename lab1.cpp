059ed70b8fad51c80810363ac0201013d4493dd0
bloodypierro <internetwarningg@gmail.com>
2025-09-26 11:09:18 +0200

    Add files via upload

--- lab1.cpp
+++ lab1.cpp
@@ -1,21 +1,21 @@
 ﻿
-    #include <iostream>
-    #include <cmath>
-    using namespace std;
+#include <iostream>
+#include <cmath>
+using namespace std;
 
 
-    int main()
-    {
-        setlocale(LC_ALL, "Russian");
-        const double P = 13.1415;
-        double radius;
-        cout << "Радиус сферы:";
-        cin >> radius;
-        double Ploshyad = 4 * P * pow(radius, 2);
-        double volume = (4.0 / 3.0) * P * pow(radius, 3);
-        cout << "\n Результат:" << endl;
-        cout << "Радиус сферы:" << radius << endl;
-        cout << "Площадь поверхности:" << Ploshyad << endl;
-        cout << "Обьем сферы:" << volume << endl;
-        return 0;
-    }
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const float P = 13.1415;
+    float radius;
+    cout << "Радиус сферы:";
+    cin >> radius;
+    float Ploshyad = 4 * P * pow(radius, 2);
+    float volume = (4.0 / 3.0) * P * pow(radius, 3);
+    cout << "\n Результат:" << endl;
+    cout << "Радиус сферы:" << radius << endl;
+    cout << "Площадь поверхности:" << Ploshyad << endl;
+    cout << "Обьем сферы:" << volume << endl;
+    return 0;
+}
